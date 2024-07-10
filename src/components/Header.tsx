7ae688b10df3089a54603821f4bc5790fe906487
= <=>
2024-07-10 21:04:06 +0530

    peoject setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+export function Header() {
+  return (
+    <>
+      <nav className="bg-[#FAFDF1] w-full h-32 ">
+        <div className="flex justify-center">
+          <div>asd</div>
+
+          <div className="mix-blend-multiply h-32">
+            <img
+              src="../../public/logo.png"
+              alt="mindmiracles"
+              width={"120"}
+              height={"120"}
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
