447da6f04b272652e6fe95ea220fd67968793a89
Dipak Khade <dipakhade214@gmail.com>
2024-12-29 22:58:43 +0530

    added top loader

--- src/components/7-days-program/PayAndRegisterButton.tsx
+++ src/components/7-days-program/PayAndRegisterButton.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Script from "next/script";
 import { Button } from "../ui/button";
@@ -6,99 +6,121 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export const PayAndRegisterButton = ({ name, email, whatsapp, age }: {
-    name: string,
-    email: string,
-    whatsapp: number,
-    age: number
+export const PayAndRegisterButton = ({
+  name,
+  email,
+  whatsapp,
+  age,
+}: {
+  name: string;
+  email: string;
+  whatsapp: number;
+  age: number;
 }) => {
+  // const [amount, setAmount] = useState<number>(1499);
+  const [loading, SetLoading] = useState<boolean>(false);
+  useEffect(() => {
+    // console.log({name,email,whatsapp,age})
+  }, [name, email, whatsapp, age]);
 
-    // const [amount, setAmount] = useState<number>(1499);
-    const [loading, SetLoading] = useState<boolean>(false)
-    useEffect(() => {
-        // console.log({name,email,whatsapp,age})
-    }, [name, email, whatsapp, age])
+  const router = useRouter();
 
-    const router = useRouter()
+  const amountToPay = 1499 * 100;
 
-    const amountToPay = 1499 * 100;
+  const createOrder = async () => {
+    SetLoading(true);
+    const res = await fetch("/api/createOrder", {
+      method: "POST",
+      body: JSON.stringify({ amount: amountToPay }),
+    });
+    const data = await res.json();
+    const paymentData = {
+      key: process.env.key_id,
+      order_id: data.id,
 
-    const createOrder = async () => {
-        SetLoading(true)
-        const res = await fetch("/api/createOrder", {
-            method: "POST",
-            body: JSON.stringify({ amount: amountToPay }),
+      handler: async function (response: any) {
+        // verify payment
+        const res = await fetch("/api/verify", {
+          method: "POST",
+          body: JSON.stringify({
+            orderId: response.razorpay_order_id,
+            razorpayPaymentId: response.razorpay_payment_id,
+            razorpaySignature: response.razorpay_signature,
+          }),
         });
         const data = await res.json();
-        const paymentData = {
-            key: process.env.key_id,
-            order_id: data.id,
-
-            handler: async function (response: any) {
-                // verify payment
-                const res = await fetch("/api/verify", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        orderId: response.razorpay_order_id,
-                        razorpayPaymentId: response.razorpay_payment_id,
-                        razorpaySignature: response.razorpay_signature,
-                    }),
-                });
-                const data = await res.json();
-                if (data.isOk) {
-                    toast.success('Registration successfull')
-                    const response = await fetch(`/api/purchase`, {
-                        method: "POST",
-                        body: JSON.stringify({
-                            name,
-                            email,
-                            whatsapp,
-                            age,
-                            amountToPay
-                        })
-                    })
-                    const res = await response.json();
-                    if (res.id) {
-                        SetLoading(false)
-                        router.push('/')
-                        toast.success('added to database')
-                    } else {
-                        SetLoading(false)
-                        toast.error(`Please Contact to Adminstrator`)
-                    }
-                } else {
-                    alert("Payment failed");
-                }
-            },
-        };
-
-        const payment = new (window as any).Razorpay(paymentData);
-        payment.open();
+        if (data.isOk) {
+          toast.success("Registration successfull");
+          const response = await fetch(`/api/purchase`, {
+            method: "POST",
+            body: JSON.stringify({
+              name,
+              email,
+              whatsapp,
+              age,
+              amountToPay,
+            }),
+          });
+          const res = await response.json();
+          if (res.id) {
+            SetLoading(false);
+            router.push("/");
+            toast.success("added to database");
+          } else {
+            SetLoading(false);
+            toast.error(`Please Contact to Adminstrator`);
+          }
+        } else {
+          alert("Payment failed");
+        }
+      },
     };
 
-    return <>
-        <Script
-            type="text/javascript"
-            src="https://checkout.razorpay.com/v1/checkout.js"
-        />
-        <Button disabled={!name || !email} onClick={createOrder} type="submit" className="w-full bg-green-700 hover:bg-[#3a5a40]">
-            {loading ?
-                <Spinner/>
-                : "Pay And Register"}
+    const payment = new (window as any).Razorpay(paymentData);
+    payment.open();
+  };
 
-        </Button>
-
+  return (
+    <>
+      <Script
+        type="text/javascript"
+        src="https://checkout.razorpay.com/v1/checkout.js"
+      />
+      <Button
+        disabled={!name || !email}
+        onClick={createOrder}
+        type="submit"
+        className="w-full bg-green-700 hover:bg-[#3a5a40]"
+      >
+        {loading ? <Spinner /> : "Pay And Register"}
+      </Button>
     </>
-}
+  );
+};
 
-const Spinner = () =>{
-    return <>
-    <div role="status">
-                    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                </div>
+const Spinner = () => {
+  return (
+    <>
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
     </>
-}
+  );
+};
+
