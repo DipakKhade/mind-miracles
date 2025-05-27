912eb19002979303f937c961faae8c7999fb74ea
Dipak Khade <dipakhade214@gmail.com>
2025-05-27 18:31:27 +0530

    added bucker dir

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,6 @@ export const AdminMails: string[] = [
   'mindmiracles1707@gmail.com',
   'dipakhade214@gmail.com',
 ];
+
+export const BUCKET_NAME = 'mindmiracles';
+export const BUCKET_COURSE_DIR = 'test-cource';
