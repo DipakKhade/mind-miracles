1827652982337d0071f016242286fa755c3cdee8
Dipak Khade <dipakhade214@gmail.com>
2025-05-27 20:57:02 +0530

    added common fns

--- src/app/api/signed-url/route.ts
+++ src/app/api/signed-url/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { BUCKET_COURSE_DIR } from '@/lib';
 import { Readable } from 'stream';
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { streamToString } from '@/lib/common-functions';
 
 const s3 = new S3Client({
   region: 'eu-north-1',
@@ -16,32 +17,35 @@ const s3 = new S3Client({
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  if (!session) return new NextResponse("Unauthorized", { status: 401 });
+  if (!session) return new NextResponse('Unauthorized', { status: 401 });
 
   let videoPath = req.nextUrl.searchParams.get('video');
+
   if (!videoPath?.endsWith('.m3u8')) {
     videoPath = `${videoPath}/output.m3u8`;
   }
-  console.log('videoPath================:', videoPath);
+
   const basePath = videoPath?.replace('/output.m3u8', '') ?? '';
-  console.log('basePath================:', basePath);
   const segmentPrefix = `test-cource/${basePath}`;
-  console.log('segmentPrefix================:', segmentPrefix);
-  const m3u8Object = await s3.send(new GetObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET_NAME!,
-    Key: `test-cource/${videoPath}`,
-  }));
-  //test-cource/test/output.m3u8
+
+  const m3u8Object = await s3.send(
+    new GetObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
+      Key: `test-cource/${videoPath}`,
+    }),
+  );
+
   const m3u8Text = await streamToString(m3u8Object.Body as Readable);
 
   //@ts-ignore
   const segmentFiles = [...m3u8Text.matchAll(/(output\d+\.ts)/g)];
-  console.log('segmentFiles:====================', segmentFiles);
 
-  const replacements = await Promise.all(segmentFiles.map(async ([filename]) => {
-    const signedUrl = await generateSignedUrl(`${segmentPrefix}/${filename}`);
-    return [filename, signedUrl];
-  }));
+  const replacements = await Promise.all(
+    segmentFiles.map(async ([filename]) => {
+      const signedUrl = await generateSignedUrl(`${segmentPrefix}/${filename}`);
+      return [filename, signedUrl];
+    }),
+  );
 
   let updatedM3u8 = m3u8Text;
   for (const [filename, url] of replacements) {
@@ -50,14 +54,5 @@ export async function GET(req: NextRequest) {
 
   return new NextResponse(updatedM3u8, {
     headers: { 'Content-Type': 'application/vnd.apple.mpegurl' },
-  });
-}
-
-async function streamToString(stream: Readable): Promise<string> {
-  return await new Promise((resolve, reject) => {
-    const chunks: any[] = [];
-    stream.on('data', chunk => chunks.push(chunk));
-    stream.on('error', reject);
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
   });
 }
