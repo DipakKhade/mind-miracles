1bdef2bbef2396ed58f3a2888b2a8c6064028327
Dipak Khade <dipakhade214@gmail.com>
2025-05-27 18:28:53 +0530

    s3 get sign url

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,8 +1,12 @@
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  GetObjectCommand,
+  HeadObjectCommand,
+} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const s3 = new S3Client({
-  region: 'us-east-1',
+  region: 'eu-north-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
@@ -10,11 +14,21 @@ const s3 = new S3Client({
 });
 
 export async function generateSignedUrl(key: string) {
-  const command = new GetObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET_NAME!,
-    Key: key,
-  });
+  try {
+    const headCommand = new HeadObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
+      Key: key,
+    });
+    await s3.send(headCommand);
+    const getCommand = new GetObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
+      Key: key,
+    });
 
-  const url = await getSignedUrl(s3, command, { expiresIn: 60 * 5 });
-  return url;
+    const url = await getSignedUrl(s3, getCommand, { expiresIn: 60 * 5 });
+    return url;
+  } catch (error) {
+    console.error('AWS S3 Error:', error);
+    throw new Error(`Failed to generate signed URL: ${error}`);
+  }
 }
