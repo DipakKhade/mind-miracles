624c3b211ff922b1d69af4934b3b56de66d868f2
Dipak Khade <dipakhade214@gmail.com>
2025-05-27 18:29:52 +0530

    added sign url route

--- src/app/api/signed-url/route.ts
+++ src/app/api/signed-url/route.ts
@@ -2,19 +2,21 @@ import { getServerSession } from 'next-auth/next';
 import { generateSignedUrl } from '@/lib/s3';
 import { authOptions } from '@/lib/auth_options';
 import { NextRequest, NextResponse } from 'next/server';
+import { BUCKET_COURSE_DIR } from '@/lib';
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
 
-  if (!session) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  if (!session)
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
   const videoId = req.nextUrl.searchParams.get('videoId');
-  const hasAccess = true;   // TODO: check if user has access to the video
+  const hasAccess = true;
 
-  if (!hasAccess) return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+  if (!hasAccess)
+    return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
 
-  const key = `test-cource/${videoId}`;
-
+  const key = `${BUCKET_COURSE_DIR}/${videoId}`;
   const url = await generateSignedUrl(key);
 
   return NextResponse.json({ url });
