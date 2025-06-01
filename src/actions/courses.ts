baaecb6a266ac4627357ab680a18d87d5dfd6777
Dipak Khade <dipakhade214@gmail.com>
2025-06-01 14:39:28 +0530

    get courses action

--- src/actions/courses.ts
+++ src/actions/courses.ts
@@ -1,9 +1,37 @@
 'use server';
 
-import db from "@/db";
-
+import db from '@/db';
 
 export async function getCourses() {
-    const data = await db.course.findMany();
-    return data;
+  const data = await db.course.findMany({
+    include: {
+      courseFeature: {
+        select: {
+          feature: true,
+        },
+      },
+    },
+  });
+  console.log('data at server', data);
+  return data;
+}
+
+export async function getCourseById(id: string) {
+  const data = await db.course.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      courseFeature: {
+        select: {
+          feature: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    throw new Error('Course not found');
+  }
+  return data;
 }
