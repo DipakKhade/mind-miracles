11e34ad721df71a0a2ded55ffcc827132d9554de
Dipak Khade <dipakhade214@gmail.com>
2025-06-01 14:38:59 +0530

    purchase route added userid

--- src/app/api/purchase/route.ts
+++ src/app/api/purchase/route.ts
@@ -2,50 +2,51 @@ import db from '@/db';
 import { courses } from '@/types';
 import { NextRequest, NextResponse } from 'next/server';
 import { FormState } from '@/store';
+import { authOptions } from '@/lib/auth_options';
+import { getServerSession } from 'next-auth';
 
 export async function POST(request: NextRequest) {
-  const { form_values, course_name, amount_to_pay } =
-    (await request.json()) as {
-      form_values: FormState;
-      course_name: courses;
-      amount_to_pay: number;
-    };
+  const session = await getServerSession(authOptions);
+  const user = await db.user.findFirst({
+    where: {
+      email: session.user.email,
+    },
+    select: {
+      id: true,
+    },
+  });
 
+  if (!user) {
+    return NextResponse.json({ message: 'Not authenticated' }, { status: 401 });
+  }
+  const { form_values, course_id, amountToPay } = (await request.json()) as {
+    form_values: FormState;
+    course_id: string;
+    amountToPay: number;
+  };
   const { name, age, whatsapp, email } = form_values;
   try {
     //FIX : amount_to_pay get it from razorpay payment response
-
-    switch (course_name) {
-      case courses['seven-day-program']:
-        const newPurchase = await db.sevenDaysProgramUser.create({
-          data: {
-            name,
-            email,
-            whatsapp: +whatsapp,
-            age: +age,
-            amountPaid: 1499,
+    const newPurchase = await db.enrollment.create({
+      data: {
+        courseId: course_id,
+        userId: user.id,
+        enrolledAt: new Date(),
+        payment: {
+          create: {
+            amount: amountToPay,
+            method: 'razorpay',
+            status: 'COMPLETED',
+            paidAt: new Date(),
           },
-        });
-        return NextResponse.json({
-          message: `Registration successful`,
-          id: newPurchase.id,
-        });
+        },
+      },
+    });
 
-      case courses['personal-couselling']:
-        const new_purchase = await db.personalCounsellingUser.create({
-          data: {
-            name,
-            email,
-            whatsapp: +whatsapp,
-            age: +age,
-            amountPaid: 99,
-          },
-        });
-        return NextResponse.json({
-          message: `Registration successful`,
-          id: new_purchase.id,
-        });
-    }
+    return NextResponse.json({
+      message: `Registration successful`,
+      id: newPurchase.id,
+    });
   } catch (error) {
     return NextResponse.json({
       message: 'Registration failed',
