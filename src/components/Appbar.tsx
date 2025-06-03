c4c33eef4ccc33b85e98e3b25cf6bcd6e03ff975
Dipak Khade <dipakhade214@gmail.com>
2025-06-03 12:12:19 +0530

    navlinks map

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -4,9 +4,15 @@ import { SignInButton } from './sign-in-button';
 import { AdminMails } from '@/lib';
 import { authOptions } from '@/lib/auth_options';
 
+const navLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/about' },
+  { name: 'Our Work', href: '/our-work' },
+  { name: 'Courses', href: '/courses' },
+  { name: 'Contact Us', href: '/contact' },
+];
 export default async function Appbar() {
   const session = await getServerSession(authOptions);
-
   return (
     <>
       <nav>
@@ -41,22 +47,15 @@ export default async function Appbar() {
             className="flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all peer-checked:mt-8 peer-checked:max-h-56 md:ml-24 md:max-h-full md:flex-row md:items-start"
           >
             <ul className="flex flex-col items-center space-y-2 font-medium md:ml-auto md:flex-row md:space-y-0">
-              <li className="hover:text-green-600 md:mr-12">
-                <a href="/#">Home</a>
-              </li>
-              <li className="hover:text-green-600 md:mr-12">
-                <a href="/#about">About</a>
-              </li>
-              <li className="hover:text-green-600 md:mr-12">
-                <a href="/#">Our Work</a>
-              </li>
-              <li className="hover:text-green-600 md:mr-12">
-                <Link href="/courses">Courses</Link>
-              </li>
-
-              <li className="hover:text-green-600 md:mr-12">
-                <a href="/#contact">Contact Us</a>
-              </li>
+              {navLinks.map((link) => (
+                <li key={link.name}>
+                  <Link href={link.href}>
+                    <span className="hover:text-green-600 md:mr-12">
+                      {link.name}
+                    </span>
+                  </Link>
+                </li>
+              ))}
 
               {session && AdminMails.includes(session.user?.email!) ? (
                 <li className="hover:text-green-600 md:mr-12">
