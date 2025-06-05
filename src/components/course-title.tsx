90ed21aeeb5d3dae830b29191c7f078956e043c2
Dipak Khade <dipakhade214@gmail.com>
2025-06-05 16:29:31 +0530

    title added

--- src/components/course-title.tsx
+++ src/components/course-title.tsx
@@ -1,15 +1,15 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft } from 'lucide-react';
 
-export default function CourseTitle({courseTitle}:{courseTitle:string}) {
-    return (
-        <div className="flex flex-row items-center justify-between">
-            <button className="pr-4" onClick={() => window.history.back()}>
-            <ArrowLeft className="h-6 w-6 text-gray-400 font-bold text-5xl" />
-            </button>
-            <div>
-                <h1 className="text-2xl font-bold text-gray-900">{courseTitle}</h1>
-                <p className="text-gray-600">Continue your learning journey</p>
-            </div>
-        </div>
-    );  
-    }
+export default function CourseTitle({ courseTitle }: { courseTitle: string }) {
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <button className="pr-4" onClick={() => window.history.back()}>
+        <ArrowLeft className="h-6 w-6 text-5xl font-bold text-gray-400" />
+      </button>
+      <div>
+        <h1 className="text-2xl font-bold text-gray-900">{courseTitle}</h1>
+        <p className="text-gray-600">Continue your learning journey</p>
+      </div>
+    </div>
+  );
+}
