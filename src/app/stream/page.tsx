c6e7813ed62ab2f1fe817c7a90292693c5beecc5
Dipak Khade <dipakhade214@gmail.com>
2025-07-08 15:26:32 +0530

    build fix6

--- src/app/stream/page.tsx
+++
@@ -1,9 +0,0 @@
-import VideoPlayer from '@/components/vedio-player';
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <VideoPlayer videoId="macros" />
-    </div>
-  );
-}
