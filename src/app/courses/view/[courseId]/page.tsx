316c8fcb23b959a4ff010479342ce98a2be4cadf
Dipak Khade <dipakhade214@gmail.com>
2025-09-20 11:52:13 +0530

    condi tnc

--- src/app/courses/view/[courseId]/page.tsx
+++ src/app/courses/view/[courseId]/page.tsx
@@ -1,11 +1,19 @@
 import { TermsAndConditions } from '@/components/7-days-program/TermsAndConditions';
 import { CourseView } from '@/components/course-view';
+import { authOptions } from '@/lib/auth_options';
+import { getServerSession } from 'next-auth';
 
-export default function Page({ params }: { params: { courseId: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <>
       <CourseView courseId={params.courseId} />
-      <TermsAndConditions />
+      {session && <TermsAndConditions />}
     </>
   );
 }
