395dac9ef3f9cb21640b38fb671d11130e9727c0
Dipak Khade <dipakhade214@gmail.com>
2025-09-20 20:46:42 +0530

    added cert id

--- src/app/api/certificate_data/route.ts
+++ src/app/api/certificate_data/route.ts
@@ -1,62 +1,58 @@
-import db from "@/db";
-import { authOptions } from "@/lib/auth_options";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import db from '@/db';
+import { authOptions } from '@/lib/auth_options';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-    try {
+  try {
+    const session = await getServerSession(authOptions);
+    const courseId = await req.nextUrl.searchParams.get('courseId');
 
-        const session = await getServerSession(authOptions)
-        const courseId = await req.nextUrl.searchParams.get('courseId')
-        
-        if(!session) {
-            return NextResponse.json({
-                success: false,
-                message: "invalid session"
-            })
-        }
-
-        if(!courseId) {
-            return NextResponse.json({
-                success: false,
-                message: "invalid course id"
-            })
-        }
-
-        const data = await db.enrollment.findFirst({
-            where: {
-                user: {
-                    email: session.user.email
-                },
-                courseId
-            },
-            select: {
-                course: {
-                    select: {
-                        title: true
-                    },
-                },
-                user: {
-                    select: {
-                        name: true
-                    }
-                },
-                certificationId: true
-            }
-        })
-
-
-        return NextResponse.json({
-            success: true,
-            message: "data retrived succesfully",
-            data
-        })
-        
-    } catch(error) {
-        return NextResponse.json({
-            success: false,
-            message: "some error occured"
-        })
+    if (!session) {
+      return NextResponse.json({
+        success: false,
+        message: 'invalid session',
+      });
     }
 
+    if (!courseId) {
+      return NextResponse.json({
+        success: false,
+        message: 'invalid course id',
+      });
+    }
+
+    const data = await db.enrollment.findFirst({
+      where: {
+        user: {
+          email: session.user.email,
+        },
+        courseId,
+      },
+      select: {
+        course: {
+          select: {
+            title: true,
+          },
+        },
+        user: {
+          select: {
+            name: true,
+          },
+        },
+        certificationId: true,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: 'data retrived succesfully',
+      data,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: 'some error occured',
+    });
+  }
 }
