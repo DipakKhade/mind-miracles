3d40a70f291001e8d574d8505f2c2f8373b018b0
Dipak Khade <dipakhade214@gmail.com>
2025-10-02 11:04:43 +0530

    bug fix

--- src/app/api/adduser/track/route.ts
+++ src/app/api/adduser/track/route.ts
@@ -1,30 +1,27 @@
-import db from "@/db";
-import { authOptions } from "@/lib/auth_options";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import db from '@/db';
+import { authOptions } from '@/lib/auth_options';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-    try {
+  try {
+    const data = await db.wpGroupMembers.findMany({
+      select: {
+        id: true,
+        name: true,
+        wpNumber: true,
+        // isAddedToGroup: true,
+      },
+    });
 
-        const data = await db.wpGroupMembers.findMany({
-            select: {
-                id: true,
-                name: true,
-                wpNumber: true,
-                isAddedToGroup: true
-            }
-        })
-
-        return NextResponse.json({
-            success: true,
-            data
-        })
-
-    } catch(error) {
-        return NextResponse.json({
-            success: false,
-            message: error
-        })
-    }
-
+    return NextResponse.json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error,
+    });
+  }
 }
