515960cca3768ef50b660bef8a3600cb727078fb
Dipak Khade <dipakhade214@gmail.com>
2025-11-20 14:50:43 +0530

    saving data

--- src/app/api/test-result/route.ts
+++ src/app/api/test-result/route.ts
@@ -1,19 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getTestResultMail } from '@/mail';
+import db from '@/db';
 
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
+    const result = await db.assessmentResult.create({
+      data: {
+        name: data.name,
+        age: Number(data.age),
+        occupation: data.occupation,
+        mobile: data.mobile,
+        email: data.email,
+        score: data.score,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+
     await getTestResultMail(data);
 
     return NextResponse.json({
       success: true,
-      message: 'Test results sent successfully',
+      id: result.id,
+      message: 'Test results saved successfully',
     });
   } catch (error) {
     return NextResponse.json({
       success: false,
-      message: 'Failed to send test results',
+      message: 'Failed to save test results',
     });
   }
 }
