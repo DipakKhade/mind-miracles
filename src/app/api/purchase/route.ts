25cded975c7b8e6948eb1658fd04d83eee0cab23
Dipak Khade <dipakhade214@gmail.com>
2026-02-09 21:51:16 +0530

    certificationId rm for debug

--- src/app/api/purchase/route.ts
+++ src/app/api/purchase/route.ts
@@ -66,7 +66,7 @@ export async function POST(request: NextRequest) {
               razorpayPaymentId,
               razorpayOrderId: orderId,
               razorpaySignature,
-              certificationId,
+              // certificationId,
             },
           },
         },
